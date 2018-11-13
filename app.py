114073d05dddc5f66807ad2a492e1155860509b2
雨落无声4617 <yuluowusheng4617@gmail.com>
2018-11-13 17:50:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,80 +1,133 @@
+# Import request module
+from urllib import request
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
 import json
+from flask import Flask
 
-#Data Dictionary is used to store data.
-Data = dict()
+app = Flask(__name__)
 
-# Prepare Fall Semester Data
-File = open("fall.html", "r")
-Content = File.read()
-soup = BeautifulSoup(Content, 'lxml')
-Result=list(soup.find_all("td"))
 
-for i in range(len(Result)):
-    xspan = Result[i].span
-    strx=str(xspan)
-    if strx.find("left: 0.0pt")!=-1:
-        # Locate all data and get them
-        CourseTitleLine = str(xspan.text)
-        CourseTitle=( CourseTitleLine.split(" ")[1].split("-")[0]  + "-" +CourseTitleLine.split(" ")[1].split("-")[1])
-        CourseName = str(Result[i+1].span.text).title()
-        CreditHours = str(Result[i + 3].span.text)
-        
-        # Save to Data Dictionary in format :{"Course Name": XXX, "Credit Hours:" :xxx}
-        Data[CourseTitle]=dict()
-        Data[CourseTitle]["CourseName"]=CourseName
-        Data[CourseTitle]["CreditHours"] = CreditHours
-        
-File.close()
+def parse_peroid(peroid):
+    # Get all the detialed data from the given period
+    type = peroid["type"]
+    instructor = peroid["instructor"]
+    start = peroid["start"]
+    end = peroid["end"]
+    location = peroid["location"]
+    days = peroid.find_all("day")
 
-# Prepare Spring Semester
-File = open("spring.html", "r")
-Content = File.read()
-soup = BeautifulSoup(Content, 'lxml')
+    # Deal with instructors.
+    instructor = instructor.split("/")
 
-Result=list(soup.find_all("td"))
-for i in range(len(Result)):
-    xspan = Result[i].span
-    strx=str(xspan)
-    # Locate all data and get them
-    if strx.find("left: 0.0pt")!=-1:
+    result = list()
 
-        CourseTitleLine = str(xspan.text)
-        CourseTitle=( CourseTitleLine.split(" ")[1].split("-")[0]  + "-" +CourseTitleLine.split(" ")[1].split("-")[1])
-        CourseName = str(Result[i+1].span.text).title()
-        CreditHours = str(Result[i + 3].span.text)
-        
-        # Save to Data Dictionary in format :{"Course Name": XXX, "Credit Hours:" :xxx}
-        Data[CourseTitle]=dict()
-        Data[CourseTitle]["CourseName"]=CourseName
-        Data[CourseTitle]["CreditHours"] = CreditHours
-        
-File.close()
+    for day in days:
+        D = dict()
+        day = int(day.text)+1
 
-# Change the data format to : 
-#    [
-#        {"Course Name": XXX, "Credit Hours:" :xxx},
-#        {"Course Name": XXX, "Credit Hours:" :xxx},
-#        .....
-#    ]
-#
-# Create the list
-Data_list = list()
+        D["type"] = type
+        D["instructor"] = instructor
+        D["start"] = start
+        D["end"] = end
+        D["location"] = location
+        D["day"] = day
 
-# Transfer data
-for x in Data:
-    tmpDict = dict()
-    tmpDict["CourseTitle"] = x
-    tmpDict["CourseName"] = Data[x]["CourseName"]
-    tmpDict["CreditHours"] = Data[x]["CreditHours"]
-    Data_list.append(tmpDict)
+        result.append(D)
 
-# Dump to a json file
-DumpString=json.dumps(Data_list, sort_keys=True, indent=2)
-writefile = open("data.json","w")
-writefile.write(DumpString)
-writefile.close()
-File.close()
+    return result
+
+
+def parse_section(section):
+    crn = section["crn"]
+    shortname = section["num"]
+    seats_taken = section["students"]
+    seats = section["seats"]
+
+    periods = section.find_all("period")
+
+    periods_list = list()
+    instructors_set = set()
+
+    for period in periods:
+        parse_peroid_result = parse_peroid(period)
+        periods_list = periods_list + parse_peroid_result
+
+        for single_period in parse_peroid_result:
+            for instructor in single_period["instructor"]:
+                if(instructor != "Staff"):
+                    instructors_set.add(instructor)
+
+    instructors_list = list(instructors_set)
+
+    result = dict()
+    result["crn"] = crn
+    result["shortname"] = shortname
+    result["seats_taken"] = seats_taken
+    result["seats"] = seats
+    result["instructors"] = instructors_list
+    result["periods"] = periods_list
+
+    return result
+
+
+
+
+def parse_course(course):
+    longname = course["name"]
+    short_name = course["num"]
+    min_credits = course["credmin"]
+    max_credits = course["credmax"]
+
+    sections = course.find_all("section")
+
+    sections_list = list()
+
+    for section in sections:
+        sections_list.append(parse_section(section))
+
+    result = dict()
+    result["longname"] = longname
+    result["shortname"] = short_name
+
+    result["min_credits"] = min_credits
+    result["max_credits"] = max_credits
+    result["sections"] = sections_list
+
+    return result
+
+
+@app.route('/<semester>')
+def return_result(semester):
+    URL = "https://sis.rpi.edu/reg/rocs/{}.xml".format(semester)
+
+    with request.urlopen(URL) as rawRequest:
+        if(str(rawRequest.status) != "200"):
+            return "Incorrect URL"
+        data = rawRequest.read()
+
+    soup = BeautifulSoup(data, 'lxml')
+    courses = soup.find_all("course")
+    D = dict()
+    subjects = list()
+
+    for course in courses:
+        shortname = course["dept"]
+        if shortname not in D:
+            D[shortname] = list()
+        D[shortname].append(parse_course(course))
+
+    for shortname in D:
+        subjects.append({"shortname": shortname, "listings": D[shortname]})
+
+    result = {"subjects": subjects}
+
+    return json.dumps(result)
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
 
 
