3760a41e58b9afd7e03bef8238ded3133ce73c4f
YLWS-4617 <yuluowusheng4617@gmail.com>
2018-11-15 16:45:39 -0500

    Update to V2 (Web API)

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # Import request module
-from urllib import request
+import urllib
 from bs4 import BeautifulSoup
 import json
 from flask import Flask
+import time
 
 app = Flask(__name__)
-
 
 def parse_peroid(peroid):
     # Get all the detialed data from the given period
@@ -22,8 +22,10 @@ def parse_peroid(peroid):
     result = list()
 
     for day in days:
+        # Create a Dictionary to store the data for different days.
         D = dict()
-        day = int(day.text)+1
+        day = int(day.text)+1   # Remember to increase the day value by 1
+
 
         D["type"] = type
         D["instructor"] = instructor
@@ -32,6 +34,7 @@ def parse_peroid(peroid):
         D["location"] = location
         D["day"] = day
 
+        # Append to the result list
         result.append(D)
 
     return result
@@ -43,20 +46,27 @@ def parse_section(section):
     seats_taken = section["students"]
     seats = section["seats"]
 
+    # Find all the sub-periods
     periods = section.find_all("period")
 
+    # Create a list to store the periods after analyzing
     periods_list = list()
+
+    # Store the professors that teach this course.
     instructors_set = set()
 
     for period in periods:
+        # Fetch  and save all the period information.
         parse_peroid_result = parse_peroid(period)
         periods_list = periods_list + parse_peroid_result
 
+        # Deal with the professor set.
         for single_period in parse_peroid_result:
             for instructor in single_period["instructor"]:
                 if(instructor != "Staff"):
                     instructors_set.add(instructor)
 
+    # Turn set to a list finnally.
     instructors_list = list(instructors_set)
 
     result = dict()
@@ -78,8 +88,10 @@ def parse_course(course):
     min_credits = course["credmin"]
     max_credits = course["credmax"]
 
+    # Find all the sub sections
     sections = course.find_all("section")
 
+    # create a list to store the parsed section information
     sections_list = list()
 
     for section in sections:
@@ -95,27 +107,31 @@ def parse_course(course):
 
     return result
 
+def fetch_data(URL):
 
-@app.route('/<semester>')
-def return_result(semester):
-    URL = "https://sis.rpi.edu/reg/rocs/{}.xml".format(semester)
+    # Try to fetch the data from the given URL,
+    # If the URL doesn't exisit, return "Incorrect URL"
+    try:
+        data=urllib.request.urlopen(URL).read()
+    except urllib.error.HTTPError as error:
+        return "Incorrect URL"
 
-    with request.urlopen(URL) as rawRequest:
-        if(str(rawRequest.status) != "200"):
-            return "Incorrect URL"
-        data = rawRequest.read()
-
+    # Create a soup for the whole page.
     soup = BeautifulSoup(data, 'lxml')
+
+    # Store all the sub-courses to courses.
     courses = soup.find_all("course")
     D = dict()
     subjects = list()
 
+    # Deal with every course
     for course in courses:
         shortname = course["dept"]
         if shortname not in D:
             D[shortname] = list()
         D[shortname].append(parse_course(course))
 
+    # Transform data type
     for shortname in D:
         subjects.append({"shortname": shortname, "listings": D[shortname]})
 
@@ -123,6 +139,17 @@ def return_result(semester):
 
     return json.dumps(result)
 
+
+@app.route('/<semester>')
+def return_result(semester):
+
+    # Check if the semester is made of digits
+    if not semester.isdigit():
+        return "Incorrect URL"
+
+    URL = "https://sis.rpi.edu/reg/rocs/{}.xml".format(semester)
+
+    return fetch_data(URL)
 
 if __name__ == '__main__':
     app.run()
